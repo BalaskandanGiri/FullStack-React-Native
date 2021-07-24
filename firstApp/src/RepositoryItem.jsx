30db088a03b13789617105be3985849f7d0c8026
BalaskandanGiri <balaskandangiri@gmail.com>
2021-07-24 10:16:01 +0530

    Adding images to item

--- firstApp/src/RepositoryItem.jsx
+++ firstApp/src/RepositoryItem.jsx
@@ -7,14 +7,29 @@ const styles = StyleSheet.create({
     },
     column: {
         flexDirection: 'column',
-    }
+    },
+    logo: {
+        width: 66,
+        height: 58,
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+    testColor: {
+        color: 'red',
+        backgroundColor: 'yellow'
+    },
+    space: {
+        justifyContent: 'space-around',
+    },
 });
 
 const RepositoryItem = ({item}) => {
     console.log(item);
     return (
-        <View style={styles.row}>
-            <View style={styles.column}>
+        <View style={styles.column}>
+            <View style={styles.row}>
                 <View>
                     <Image
                         style={styles.tinyLogo}
@@ -24,25 +39,28 @@ const RepositoryItem = ({item}) => {
                     />
                 </View>
                 <View>
-                    <Text style={{color: 'blue'}}>FullName: {item.fullName}</Text>
-                    <Text>Description: {item.description}</Text>
-                    <Text>Language: {item.language}</Text>
+                    <Text style={{color: 'blue'}}>{item.fullName}</Text>
+                    <Text style={{ flexShrink: 1 }}>{item.description}</Text>
+                    <Text style={{color:'white', backgroundColor:'blue', flexGrow: 0}}>{item.language}</Text>
                 </View>
             </View>
-            <View>
-                <Text>asd</Text>   
-                <Image
-                    style={styles.tinyLogo}
-                    source={{
-                    uri: 'https://reactnative.dev/img/tiny_logo.png',
-                    }}
-                />
-                <Image
-                    style={styles.logo}
-                    source={{
-                    uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-                    }}
-                />
+            <View style={[styles.row, styles.space]}>
+                <View style={styles.column}>
+                    <Text>{item.stargazersCount}</Text>
+                    <Text>Stars</Text>
+                </View>
+                <View>
+                    <Text>{item.forksCount}</Text>
+                    <Text>Forks</Text>
+                </View>
+                <View>
+                    <Text>{item.reviewCount}</Text>
+                    <Text>Reviews</Text>
+                </View>
+                <View>
+                    <Text>{item.ratingAverage}</Text>
+                    <Text>Ratings</Text>
+                </View>
             </View>
         </View>
         // <View style={{backgroundColor: 'pink'}}>
